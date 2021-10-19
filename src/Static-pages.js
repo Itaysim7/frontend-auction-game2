76256231a3a9c67f8978b84cfabc4904eb3ad5cf
itaysim7 <itaysim7@gmail.com>
2021-10-19 21:25:53 +0300

    hint page

--- src/Static-pages.js
+++ src/Static-pages.js
@@ -10,7 +10,7 @@ function Desc(props)
     const [page, setPage] = useState(1);
 
     const nextClicked = () => {
-        if(page>=18)
+        if(page>=19)
             window.location.href = `/forms/${props.match.params.id}`;
         else
             setPage(page+1);
@@ -27,7 +27,7 @@ function Desc(props)
         <img style={{ marginLeft: '15%'}} className="photo" src={process.env.PUBLIC_URL + `/${page}.png`} alt="logo" />
         <div style={{marginLeft: '30%', marginTop: '50px'}}>
             <h6 style={{ color: 'blue'}}>Introduction progress bar</h6>
-            <ProgressBar labelAlignment="oucetside" height="50%"  labelSize="18px" bgColor="blue" width="50%"  completed={Math.floor(((page-1)/17)*100)} />
+            <ProgressBar labelAlignment="oucetside" height="50%"  labelSize="18px" bgColor="blue" width="50%"  completed={Math.floor(((page-1)/18)*100)} />
         </div>
         <div className="buttons-container">
             {page>1 && <Button className="previous"  variant="outline-primary" size="lg"
